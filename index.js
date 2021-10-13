dcdde6a6901844d0137b53a55bb15f5551028f74
Wing <eamonn.kearns@so-4pt.net>
2021-10-13 22:23:12 +0100

    Started in on boons

--- index.js
+++ index.js
@@ -618,8 +618,9 @@ function rejectBoon()
     try
     {
         let boonCountStmt = db.prepare('SELECT COUNT(idBoons) AS cnt FROM boons WHERE idBoons=? AND bitFrom=? AND validated=0 AND acknowledged=0');
-        let boonCountQry=boonCountStmt.exec(registers.id.value, registers.player.value);
-        console.log(boonCountQry)
+        let boonCountQry=boonCountStmt.get(registers.id.value, registers.player.value);
+        console.log(boonCountQry);
+
         if(boonCountQry.cnt < 1)
         {
             respond(-1);
